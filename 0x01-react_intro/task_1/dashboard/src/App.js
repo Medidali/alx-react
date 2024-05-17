02cb02d5ac0d801713429630f19a4a4f9abaf49e
Medidali <m.mohamedidali@gmail.com>
2024-05-17 13:29:08 +0100

    react_intro2

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,25 @@
+// task_1/dashboard/src/App.js
+
+import React from 'react';
+import logo from './holberton_logo.png';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>&copy; {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,12 @@
+// task_1/dashboard/src/Notifications.js
+
+import React from 'react';
+import './Notifications.css';
+
+export default function Notifications() {
+  return (
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+    </div>
+  );
+}
